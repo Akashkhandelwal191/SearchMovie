7f290f8c2759593113d02b28f198a7b406ac688f
Akash Khandelwal <skykhandelwal1999@gmail.com>
2021-07-02 09:47:40 +0530

    Done

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header=()=>{
+    return(
+        <>
+        <div className="navbar">
+            <h1>Search Your Favourite Movie</h1>
+        </div>
+        </>
+    )
+}
+
+export default Header;
